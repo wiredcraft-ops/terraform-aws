08f3e35ac4080bcc25c5edc9dc3be0e7c8af4e4d
xuqingfeng <qingfeng@wiredcraft.com>
2020-03-02 15:27:21 +0800

    Fix syntax

--- devops/terraform/iam.tf
+++ devops/terraform/iam.tf
@@ -50,17 +50,17 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.node.name
+  role       = aws_iam_role.demo-node.name
 }
 
 resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.node.name
+  role       = aws_iam_role.demo-node.name
 }
 
 resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.node.name
+  role       = aws_iam_role.demo-node.name
 }
 
 resource "aws_iam_instance_profile" "demo-node" {
