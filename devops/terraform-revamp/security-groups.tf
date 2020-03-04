7d26aef7bbf2bf5da4f14ce124a6be9c689655bf
xuqingfeng <qingfeng@wiredcraft.com>
2020-03-04 14:47:23 +0800

    Add security group rule for allowing ssh

--- devops/terraform-revamp/security-groups.tf
+++ devops/terraform-revamp/security-groups.tf
@@ -1,1 +1,9 @@
 # allow ssh connection from WCL office
+resource "aws_security_group_rule" "allow-ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = var.wcl-ips
+  security_group_id = aws_vpc.demo.default_security_group_id
+}
--- devops/terraform-revamp/vars.tf
+++ devops/terraform-revamp/vars.tf
@@ -13,3 +13,8 @@ variable "public-subnet-cidr" {
 variable "private-subnet-cidr" {
   default = "172.16.10.0/24"
 }
+
+variable "wcl-ips" {
+  type    = list(string)
+  default = ["222.66.90.82/32", "103.220.77.16/32"]
+}
