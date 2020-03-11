8ee8fa2c53584bf2dde030d0c959f0090a219d49
xuqingfeng <qingfeng@wiredcraft.com>
2020-03-11 21:46:17 +0800

    Fix protocol

--- devops/terraform-revamp/security-groups.tf
+++ devops/terraform-revamp/security-groups.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "allow-http" {
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # allow connect to eks(control panel + worker)
   }
 
